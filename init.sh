8d09cd922d3e8302fbf0dd21fc7a6bd957d4070d
pietrodn <powerpdn@gmail.com>
2017-01-15 08:35:01 +0100

    Fix to init.sh: runAll would only run the first script

--- init.sh
+++ init.sh
@@ -16,8 +16,8 @@ SCRIPTS=(\"ContributingFactors\" \"LethalPerWeek\" \"WeekBorough\")
 
 
 usage (){
-echo \"Usage: \$0 [\"\${SCRIPTS[*]} \"]| runAll\"
-exit 1
+    echo \"Usage: \$0 [\"\${SCRIPTS[*]} \"] | runAll\"
+    exit 1
 }
 
 if [ \$# -lt 1 ]; then
@@ -27,7 +27,7 @@ fi
 rm -rf $CURRENT_PATH/data/output
 
 if [ \$1 == \"runAll\" ]; then
-    for i in \$SCRIPTS; do
+    for i in \"\${SCRIPTS[@]}\"; do
          hadoop jar build/libs/hadoop-accidents-1.0-SNAPSHOT.jar mw.hadoop.queries.\$i file:///$CURRENT_PATH/data/NYPD_Motor_Vehicle_Collisions.csv file:///$CURRENT_PATH/data/output/\$i
     done
 else
